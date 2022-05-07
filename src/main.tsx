5253c7fb48570a7997114ae2e1bbaa7e99fbff69
EmilBert <carlemilamandus@gmail.com>
2022-05-07 22:23:05 +0200

    Animated background!

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,19 @@ import HomeView from './views/HomeView'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { ParticipantProvider } from './contexts/ParticipantContext';
 
+
 const theme = createTheme({
 	typography: {
-	 "fontFamily": `"Roboto", "Helvetica", "Arial", sans-serif`,
-	 "fontSize": 14,
-	 "fontWeightLight": 300,
-	 "fontWeightRegular": 400,
-	 "fontWeightMedium": 500
+		"fontFamily": `"Roboto", "Helvetica", "Arial", sans-serif`,
+		"fontSize": 14,
+		"fontWeightLight": 300,
+		"fontWeightRegular": 400,
+		"fontWeightMedium": 500
 	}
- });
+});
+
+import './style.scss';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
