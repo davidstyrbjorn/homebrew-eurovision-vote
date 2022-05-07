0c08019139a700fa3e2b6c85d59bd710234fd068
kraken-dave-windows <davidstyrbjorn@gmail.com>
2022-05-07 17:52:44 +0200

    new type on User

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,25 @@
 import React from "react"
 
+export type Vote = {
+    country: string,
+    rating: number
+}
+
 export type User = {
     name: string,
-    // ... votes here later
+    votes: Vote[]
 }
 
 export type UserContextType = {
     user: User,
     setUser: React.Dispatch<React.SetStateAction<User>>
+}
+
+export type Participant = {
+    artist: string,
+    block: string,
+    country: string,
+    language: string,
+    region: string,
+    title: string
 }
