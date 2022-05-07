a7617f0266fa31690d61fc76c59c265603cdf2f5
kraken-dave-windows <davidstyrbjorn@gmail.com>
2022-05-07 17:52:57 +0200

    new firebase function for participants + user

--- src/firebase/participants.ts
+++ src/firebase/participants.ts
@@ -1,22 +1,13 @@
 import { db } from "../firebase.config";
 import participants from "../euro2022.json";
-import { collection, doc, setDoc, writeBatch } from "@firebase/firestore";
-
-/*
-            artist: entry.Artist,
-            block: entry.Block,
-            country: entry.Land,
-            language: entry.Land,
-            region: entry.Region,
-            title: entry.Bidrag
-*/
+import { collection, doc, getDocs, setDoc, writeBatch } from "@firebase/firestore";
+import { Participant } from "../types";
 
 const syncParticipantsWithFirestore = async() => {
     try {
-        console.log("TJA");
         const batch = writeBatch(db);
         // Grab ref, and create btach 
-        participants.forEach((entry) => {
+        participants.forEach((entry, idx) => {
             const newDocRef = doc(collection(db, 'participants'));
             batch.set(newDocRef, {
                 artist: entry.Artist,
@@ -24,7 +15,8 @@ const syncParticipantsWithFirestore = async() => {
                 country: entry.Land,
                 language: entry.Land,
                 region: entry.Region,
-                title: entry.Bidrag
+                title: entry.Bidrag,
+                order: idx
             });
         })
 
@@ -33,7 +25,16 @@ const syncParticipantsWithFirestore = async() => {
     }catch(e){
         console.log("Batch failed: ", e);
     }
-    
 }
 
-export {syncParticipantsWithFirestore}
+const getParticipantsFromFirestore = async() => {
+    const querySnapshot = await getDocs(collection(db, 'participants'));
+    const result: Participant[] = [];
+    querySnapshot.forEach((doc) => {
+        result.push(doc.data() as Participant);
+    })
+
+    return result;
+}
+
+export {syncParticipantsWithFirestore, getParticipantsFromFirestore}
