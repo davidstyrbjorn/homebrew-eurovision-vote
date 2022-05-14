a51d6ee6b2b1e823fc0e74b9f532d80881802c83
EmilBert <carlemilamandus@gmail.com>
2022-05-14 10:55:01 +0200

    Animations added for achivements.

--- src/views/AchievementView.tsx
+++ src/views/AchievementView.tsx
@@ -1,6 +1,8 @@
 import { Box, Typography } from "@mui/material";
 import { flexbox } from "@mui/system";
 import React, { useContext } from "react";
+import { AiFillTrophy } from "react-icons/ai";
+import { BiMedal } from "react-icons/bi";
 import QuestionSubmitPrompt from "../components/QuestionSubmitPrompt";
 import { ParticipantContext } from "../contexts/ParticipantContext";
 import { QuestionContext } from "../contexts/QuestionContext";
@@ -11,28 +13,37 @@ import AdminView from "./AdminView";
 import LoginView from "./LoginView";
 
 const containerStyle = {
-    borderRadius: "20px",
-    backgroundColor: "#fafafa",
     display: "flex",
+    flexDirection: "column",
     padding:"2rem",
-    justifyContent:"space-between", 
+    justifyContent:"center", 
+   
+    color:"white"
 }
 
+const iconStyle = {
+    display: "inline-block",
+    verticalAlign: "middle",
+    paddingBottom: "1rem"
+}
 
 const AchievementView: React.FC<{}> = () => {
     
+
+
+
+    const toFadeOut = false;
     return ( 
-        <Box sx={{display:"flex",margin:"6rem", justifyContent:"center"}} height="60vh">
-            
-            <Box display={"flex"} flexDirection="column" width="70%" justifyContent={"center"}>
-                <Typography variant="h1" color="white" sx={{fontSize:"200px" ,fontWeight:"bold"}}>RYSK SPION</Typography>
-                <Typography sx={{width:"50%", color:"white"}} variant="h3">Spelaren som gav mest poäng till "vi hatar varandra men röstar ändo på varandra" gruppen</Typography>
+        <Box className={toFadeOut ? "fade-out":""} sx={{display:"flex",margin:"6rem", justifyContent:"center"}} height="70vh">
+            <Box className="title-in" display={"flex"} flexDirection="column" width="60%" justifyContent={"center"}>
+                <Typography variant="h1" color="white" sx={{fontSize:"180px" ,fontWeight:"bold"}}>RYSK SPION</Typography>
+                <Typography sx={{ color:"white"}} variant="h3">Spelaren som gav mest poäng till "vi hatar varandra men röstar ändo på varandra" gruppen</Typography>
             </Box>
             <Box sx={containerStyle}>
-                <Box>
-                    <Typography className="firstPlace"  variant="h3" >Emil</Typography>
-                    <Typography className="secondPlace" variant="h3">David</Typography>
-                    <Typography className="thirdPlace"  variant="h3" >Ruben</Typography>
+                <Box sx={{padding:"2rem", width:"fit-content"}}>
+                    <Typography className="first"  variant="h1"><AiFillTrophy   style={iconStyle}/>Emil</Typography>
+                    <Typography className="second" variant="h1"><BiMedal        style={iconStyle}/>David</Typography>
+                    <Typography className="third"  variant="h1"><BiMedal        style={iconStyle}/>Ruben</Typography>
                 </Box>
             </Box>
 
